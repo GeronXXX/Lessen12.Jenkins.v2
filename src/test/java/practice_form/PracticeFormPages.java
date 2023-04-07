58b3358094829f5af9c7cd67a9b5597603058486
GeronXXX <lityagin.ru@gmail.com>
2023-04-07 19:00:13 +0300

    v2.6

--- src/test/java/practice_form/PracticeFormPages.java
+++ src/test/java/practice_form/PracticeFormPages.java
@@ -1,5 +1,6 @@
 package practice_form;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.PageObgects;
@@ -12,6 +13,7 @@ public class PracticeFormPages extends TestBase {
     PageObgects pageObgects = new PageObgects();
 
     @Test
+    @DisplayName("Test form")
     @Tag("remote")
     void practiceFormTest() {
 
